df3009175461bc0162e86191cccea847d2b1351f
thengo1 <tbn2109@columbia.edu>
2020-12-01 17:25:39 -0500

    incrementing version

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ copyright = "2020, The Ngo"
 author = "The Ngo"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
